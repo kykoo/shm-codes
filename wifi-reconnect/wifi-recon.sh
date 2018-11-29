d25d902b0ee6cb16b3d6fe1694586d1399ad867c
kykoo <dr.ki.koo@gmail.com>
2018-11-29 13:28:09 +0000

    adxl362 logging support

--- wifi-reconnect/wifi-recon.sh
+++ wifi-reconnect/wifi-recon.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
+
+# Add following to your crontab
+# * * * * * /home/pi/codes/shm-codes/wifi-reconnect/wifi-recon.sh
+
+
 if ! [ "$(ping -c 1 192.168.3.1)" ]; then
     /usr/bin/sudo /sbin/ifdown wlan0
     /usr/bin/sudo /sbin/ifup wlan0
