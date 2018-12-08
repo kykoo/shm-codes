1567262bfdbba16130921f2e33f9c7e8d3cd7d8f
kykoo <dr.ki.koo@gmail.com>
2018-12-08 10:44:39 +0000

    restructuring

--- python_learning/1-blink-test/build-mega2560A/core/WMath.d
+++
@@ -1,2 +0,0 @@
-build-mega2560A/core/WMath.o: \
- /usr/share/arduino/hardware/arduino/cores/arduino/WMath.cpp
--- python_learning/1-blink-test/build-mega2560A/core/new.d
+++
@@ -1,3 +0,0 @@
-build-mega2560A/core/new.o: \
- /usr/share/arduino/hardware/arduino/cores/arduino/new.cpp \
- /usr/share/arduino/hardware/arduino/cores/arduino/new.h
